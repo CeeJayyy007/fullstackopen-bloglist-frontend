dc2330a504f268d86ccc822cac61ee23e66e8431
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-06 09:56:12 -0500

    complete 5.2: bloglist frontend step 2

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -14,11 +14,22 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedInUser = window.localStorage.getItem("loggedInUser");
+
+    if (loggedInUser) {
+      setUser(JSON.parse(loggedInUser));
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
     try {
       const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem("loggedInUser", JSON.stringify(user));
+
       setUser(user);
       setUsername("");
       setPassword("");
@@ -27,7 +38,10 @@ const App = () => {
     }
   };
 
-  console.log("user", user);
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedInUser");
+    setUser(null);
+  };
 
   if (user === null) {
     return loginForm(handleLogin, setUsername, setPassword, username, password);
@@ -38,7 +52,8 @@ const App = () => {
       <div>
         <h2>blogs</h2>
         <p>
-          <strong>{user.name}</strong> logged in
+          <strong>{user.name}</strong> logged in{" "}
+          <button onClick={handleLogout}>Logout</button>
         </p>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
