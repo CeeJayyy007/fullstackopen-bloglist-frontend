3eed5b8ce082fdabe3617b3a5a090d0eaaf0db0b
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-06 09:41:15 -0500

    complete 5.1: bloglist frontend

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,20 +1,49 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs.js";
+import loginService from "./services/login.js";
+import loginForm from "./components/Form";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log("wrong credentials");
+    }
+  };
+
+  console.log("user", user);
+
+  if (user === null) {
+    return loginForm(handleLogin, setUsername, setPassword, username, password);
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      <div>
+        <h2>blogs</h2>
+        <p>
+          <strong>{user.name}</strong> logged in
+        </p>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
     </div>
   );
 };
