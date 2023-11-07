e1a6d167b2d9535ed4b034f66eeeec5872af099f
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-07 07:01:08 -0500

    complete 5.7:: blog list frontend step 7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} | {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false);
+
+  const blogStyle = {
+    padding: "10px 0px 10px 5px",
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleView = () => {
+    setView(!view);
+    console.log("button clicked");
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} | {blog.author}{" "}
+        <button onClick={handleView}>{view ? "hide" : "view"}</button>
+      </div>
+
+      <div style={{ display: view ? " " : "none" }}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          {"   "}
+          <button>like</button>
+        </div>
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
