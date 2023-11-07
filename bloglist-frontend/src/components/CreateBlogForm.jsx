22ce54b5d6fc25f30e2e8171d0e9ab5708567f5f
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-07 06:07:40 -0500

    separate states to create blog form

--- bloglist-frontend/src/components/CreateBlogForm.jsx
+++ bloglist-frontend/src/components/CreateBlogForm.jsx
@@ -1,32 +1,49 @@
 import Title from "./Title";
+import { useState } from "react";
 
 // add new blog
-const CreateBlogForm = ({
-  handleCreateBlog,
-  handleTitleChange,
-  handleAuthorChange,
-  handleUrlChange,
-  title,
-  author,
-  url,
-}) => {
+const CreateBlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleCreateBlog = (event) => {
+    event.preventDefault();
+
+    createBlog({
+      title: title,
+      author: author,
+      url: url,
+    });
+
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+  };
+
   return (
     <div>
       <Title title="Create new blog" />
       <form onSubmit={handleCreateBlog}>
         <div>
           Title:
-          <input value={title} onChange={handleTitleChange} />
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
         </div>
 
         <div>
           Author:
-          <input value={author} onChange={handleAuthorChange} />
+          <input
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
         </div>
 
         <div>
           URL:
-          <input value={url} onChange={handleUrlChange} />
+          <input value={url} onChange={({ target }) => setUrl(target.value)} />
         </div>
 
         <button type="submit">Create</button>
